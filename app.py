7d63887018a60e0a6a54b03a2381968bfaea8e66
Xinyu Li <tempojd1221@gmail.com>
2025-04-14 23:04:12 -0400

    index+studies+portrait+viewer updated

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/studies')
+def studies():
+    return render_template('studies.html')
+
+@app.route('/portrait')
+def portrait():
+    return render_template('portrait.html')
+
+@app.route('/viewer')
+def viewer():
+    return render_template('viewer.html')
+
+@app.route('/help')
+def help_page():
+    return render_template('help.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
